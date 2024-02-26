236a77fa90fd3905be735b25ab9641199e590974
washira5lab <washira@5lab.co>
2024-02-26 16:07:19 +0700

    ✨ feat(route.ts): add PUT and DELETE functions to handle updating and deleting posts
    The PUT function allows updating a post by its ID. It receives the request and the ID of the post to be updated, and updates the title and content of the post in the database.
    
    The DELETE function allows deleting a post by its ID. It receives the request and the ID of the post to be deleted, and deletes the post from the database.

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -15,3 +15,36 @@ export async function GET(
   })
   return Response.json(post)
 }
+
+// function PUT
+export async function PUT(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const postId = Number(params.id)
+  const { title, content } = await request.json()
+  const updatedPost = await prisma.post.update({
+    where: {
+      id: postId
+    },
+    data: {
+      title,
+      content
+    }
+  })
+  return Response.json(updatedPost)
+}
+
+// function DELETE
+export async function DELETE(
+  request: Request,
+  { params }: { params: { id: string } }
+) {
+  const postId = Number(params.id)
+  const deletedPost = await prisma.post.delete({
+    where: {
+      id: postId
+    }
+  })
+  return Response.json(deletedPost)
+}
