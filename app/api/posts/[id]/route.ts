5379d8a985e0d774bae3f928b45b552e13329f7c
washira5lab <washira@5lab.co>
2024-02-26 16:15:58 +0700

    🐛 fix(route.ts): add error handling to GET, PUT, and DELETE functions to return a 404 response if an error occurs ✨ feat(route.ts): add try-catch blocks to handle errors and return a 404 response with the error message

--- app/api/posts/[id]/route.ts
+++ app/api/posts/[id]/route.ts
@@ -7,13 +7,17 @@ export async function GET(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const postId = Number(params.id)
-  const post = await prisma.post.findUnique({
-    where: {
-      id: postId
-    }
-  })
-  return Response.json(post)
+  try {
+    const postId = Number(params.id)
+    const post = await prisma.post.findUnique({
+      where: {
+        id: postId
+      }
+    })
+    return Response.json(post)
+  } catch (error) {
+    return new Response(error as BodyInit, { status: 404 })
+  }
 }
 
 // function PUT
@@ -21,30 +25,37 @@ export async function PUT(
   request: Request,
   { params }: { params: { id: string } }
 ) {
-  const postId = Number(params.id)
-  const { title, content } = await request.json()
-  const updatedPost = await prisma.post.update({
-    where: {
-      id: postId
-    },
-    data: {
-      title,
-      content
-    }
-  })
-  return Response.json(updatedPost)
+  try {
+    const postId = Number(params.id)
+    const { title, content } = await request.json()
+    const updatedPost = await prisma.post.update({
+      where: {
+        id: postId
+      },
+      data: {
+        title,
+        content
+      }
+    })
+    return Response.json(updatedPost)
+  } catch (error) {
+    return new Response(error as BodyInit, { status: 404 })
+  }
 }
 
 // function DELETE
 export async function DELETE(
-  request: Request,
   { params }: { params: { id: string } }
 ) {
-  const postId = Number(params.id)
-  const deletedPost = await prisma.post.delete({
-    where: {
-      id: postId
-    }
-  })
-  return Response.json(deletedPost)
+  try {
+    const postId = Number(params.id)
+    const deletedPost = await prisma.post.delete({
+      where: {
+        id: postId
+      }
+    })
+    return Response.json(deletedPost)
+  } catch (error) {
+    return new Response(error as BodyInit, { status: 404 })
+  }
 }
