d08dedded55b1fab8f3e12363335c8d75e9d7957
washira5lab <washira@5lab.co>
2024-02-26 12:36:22 +0700

    🔧 fix(route.ts): remove unused import and add missing newline at end of file for better code readability ✨ feat(route.ts): add POST function to handle creating new posts and saving them to the database using Prisma ORM

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -1,7 +1,34 @@
-// ใช้ GET() เพื่อเป็นการขอใช้ method GET ผ่าน api/post
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
+
+// function GET
+
+// ใช้ GET() เพื่อเป็นการขอใช้ method GET ผ่าน api/post
 export function GET() {
   return Response.json({
     message: 'test'
   })
 }
+
+// function POST
+export async function POST(request: Request) {
+  const { title, content } = await request.json()
+  const newPost = await prisma.post.create({
+    data: {
+      title,
+      content
+    }
+  })
+  // newPost คือข้อมูลที่เพิ่งสร้างขึ้น จะได้ id มาด้วย
+  // ส่งข้อมูลกลับไปให้ผู้ใช้
+  // return Response.json({
+  //   data: {
+  //     title,
+  //     content
+  //   }
+  // })
+  return Response.json(newPost)
+}
+
+// function PUT
